7e5bef80f69c60e979ce702c2aeaffac869f8f0d
UsielOM <usiel31197@outlook.es>
2023-05-22 20:48:44 -0600

    Primeras constantes

--- pruebaAldo.js
+++ pruebaAldo.js
@@ -29,7 +29,9 @@ function fakeAPI() {
   
   requestWithTimeout();
 
+const fakeAPIStub = sinon.stub().resolves("Hello world");//Cree la constante fakeApiStub para simular la primera respuesta con el resolve para que sea exitosa 
 
+const timeoutAfter = sinon.stub().rejects(new Error("request timed-out"));//Cree la consatnte en caso de error 
 
 
 
