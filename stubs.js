5b8ee24b667ab17a160abbccc84d72e20735e809
UsielOM <usiel31197@outlook.es>
2023-05-22 20:34:05 -0600

    Implementacion de calledTwice y reset

--- stubs.js
+++ stubs.js
@@ -1,8 +1,9 @@
 import Sinon from "sinon";
 
 function obtenerDatos() {
+
 return {
-    nombre: 'Usiel Orihuela',
+    nombre: 'Usiel Orihuela Monroy',
     edad: 30,
 };
 
@@ -15,3 +16,18 @@ const stub = Sinon.stub().returns({
 
 const datos = stub();
 console.log(datos);
+
+stub.returns({
+    nombre: 'Usiel Orihuela',
+    edad: 45,
+});
+
+const datos2 = stub();
+console.log(datos2);
+
+Sinon.assert.calledTwice(stub);// Verifica que el stub haya sido llamado exactamente dos veces
+
+stub.reset();//Me devolvera mis datos originales 
+
+const datos3 = obtenerDatos();
+console.log(datos3);
